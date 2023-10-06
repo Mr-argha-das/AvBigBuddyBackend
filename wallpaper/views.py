c6759b2699d7c0370f933d3c3208684009565ab2
arghaahit <argha.ahit@gmail.com>
2023-10-06 19:16:33 +0530

    h

---
+++ wallpaper/views.py
@@ -0,0 +1,29 @@
+from .models import User
+from .serailizer import UserSerializers
+from rest_framework import status
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView
+)
+from rest_framework.response import Response
+
+
+class UserLsit(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers  # Individual object serializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = UserSerializers(queryset, many=True)  # Use UserSerializer to serialize a list
+        return Response(serializer.data)
+
+
+class UserAdd(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    def create(self, request, *args, **kwargs):
+       serializer = UserSerializers(data=request.data)
+       if serializer.is_valid():
+           serializer.save() 
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
